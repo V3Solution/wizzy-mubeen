e78cc9eb8b59a084c825161de595dae427390027
17-NTU-1228 <17ntu1228@student.ntu.edu.pk>
2021-10-12 19:03:04 +0500

    ten commit wizzy app

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('./startUp/db')()
 require('./startUp/validation')()
 require('./startUp/config')()
 
-
+console.log( process.env.PORT_NUMBER,"Given Port Number...")
 const port = process.env.PORT || process.env.PORT_NUMBER;
 app.listen(port,()=>{
     console.log(`Your App Will Be Listening On The Given Port Number ${port}...`)
